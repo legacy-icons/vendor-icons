8a4e4f845536ae9410e8dc11ad2da4efe5a863fc
T1st3 <contact@tiste.org>
2015-03-11 00:21:54 +0100

    feat: add a gulp task: gulp info

--- gulpfile.js
+++ gulpfile.js
@@ -111,3 +111,39 @@ gulp.task('build', ['build_img'], function (cb) {
     displayCowsay('gulp build - DONE', cb);
   });
 });
+
+
+/*
+ * INFO TASK
+ */
+
+gulp.task('info', ['figlet'], function (cb) {
+  var txt;
+  console.log('\n\n');
+  console.log('[' + chalk.green('NAME') + '] ' + pkg.name);
+  console.log('[' + chalk.green('DESCRIPTION') + '] ' + pkg.description);
+  console.log('[' + chalk.green('VERSION') + '] ' + pkg.version);
+  console.log('[' + chalk.green('HOMEPAGE') + '] ' + pkg.homepage);
+  console.log('[' + chalk.green('GITHUB REPOSITORY') + '] ' + pkg.repository.url);
+  console.log('[' + chalk.green('NPM URL') + '] https://npmjs.org/package/' + pkg.name);
+  console.log('[' + chalk.green('BOWER URL') + '] http://bower.io/search/?q=' + pkg.name);
+  console.log('[' + chalk.green('BUG TRACKER') + '] ' + pkg.bugs.url);
+  console.log('\n');
+  txt = '[' + chalk.green('DOWNLOAD LATEST') + '] ';
+  txt += 'https://github.com/' + pkgAuthor.name + '/' + pkg.name + '/archive/master.zip';
+  console.log(txt);
+  txt = '[' + chalk.green('ALL VERSION TAGS') + '] ';
+  txt += 'https://github.com/' + pkgAuthor.name + '/' + pkg.name + '/tags';
+  console.log(txt);
+  txt = '[' + chalk.green('RSS/ATOM FOR VERSION TAGS') + '] ';
+  txt += 'https://github.com/' + pkgAuthor.name + '/' + pkg.name + '/tags.atom';
+  console.log(txt);
+  console.log('\n\n');
+  qrcode.generate(pkg.homepage);
+  console.log('\n\n');
+  triggerNotification ('Info', 'Rendered the info...', function () {
+    displayCowsay('gulp info - DONE', cb);
+  });
+});
+
+gulp.task('default', ['info', 'build']);
